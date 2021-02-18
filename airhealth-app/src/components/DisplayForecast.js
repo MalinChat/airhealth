c58cb08977a516e8d74dddba78885ffa83279b2c
MalinChat <malin.ec.svensson@gmail.com>
2021-02-18 20:12:22 +0100

    Displaying the forecast in a table, still working on the map function

--- airhealth-app/src/components/DisplayForecast.js
+++ airhealth-app/src/components/DisplayForecast.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import "./DisplayResults.css";
 
 
+
 function DisplayForecast(props){
 
     let forecast = props.forecast;
@@ -12,8 +13,28 @@ function DisplayForecast(props){
     return(
         
             <div className="DisplayForecast">
-                <p>The air quality forecast for {forecast.data.city.name} is: {forecast.data.forecast.daily.pm25[0].avg} </p>
+                <p>The air quality forecast for {forecast.data.city.name} is:</p>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Date</th>
+                                <th>AQI</th>
+                            </tr>
+                            {
+                             forecast.data.forecast.daily.pm25.map(elem => (
+                            
+                                <tr>
+                                    <td key={elem.forecast}>{forecast.data.forecast.daily.pm25[0].day}</td>
+                                    <td key={elem.forecast}>{forecast.data.forecast.daily.pm25[0].avg}</td>
 
+                                </tr>
+                             ))
+                            }
+
+
+
+                        </tbody>
+                    </table>
             </div>
         
     );
